92bb0dd813a6a1b072d924f6252f12e1e2722717
wonjaechoi <wvvwvwwv@naver.com>
2016-12-31 22:20:16 +0900

    이펙트 asset 추가, 흑형 작업중

---
+++ Assets/Scripts/Graphic/DoctorGraphicConrtoller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoctorGraphicConrtoller : MonoBehaviour {
+
+	private Animator lowerAnimator;
+	private Animator upperAnimator;
+
+	void Start () {
+		lowerAnimator = transform.FindChild ("Offset").FindChild ("Pivot").GetComponent<Animator> ();
+		upperAnimator = lowerAnimator.transform.FindChild ("body").GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			lowerAnimator.Play("Jet");
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			lowerAnimator.Play("LongJump");
+		}
+	}
+}
