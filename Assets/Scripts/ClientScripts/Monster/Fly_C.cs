f969eecc2a1d6453c3fba81aa2ff1c81ac1ac734
ghdtkazosel <ohyan19@gmail.com>
2017-02-19 19:49:25 +0900

    temp

---
+++ Assets/Scripts/ClientScripts/Monster/Fly_C.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly_C : ClientMonster {
+	public FlyGraphicController gcFly;
+
+	//private 
+
+	public override void OnRequested (){
+		base.OnRequested();
+
+		StartCoroutine(WakeUpRoutine());
+	}
+
+	private IEnumerator WakeUpRoutine(){
+		// 일어난 뒤 1sec > 애니메이션 재생 > 1sec > 무적판정 끝
+		// wakeup animation 재생 중 바로 죽을때 애니메이션이 씹히는 이슈 때문
+
+		yield return new WaitForSeconds(1);
+
+		gcFly.WakeUp ();
+
+		yield return new WaitForSeconds (1);
+
+		hTrigger.gameObject.SetActive (true);
+	}
+
+	public override void OnDie (){
+		base.OnDie ();
+
+		gcFly.Die();
+
+		ReturnObject(8);
+	}
+
+	protected override void MonsterFreeze(){
+		gcFly.AnimationFreeze ();
+	}
+
+	protected override void MonsterFreezeEnd(){
+		gcFly.AnimationResume ();
+	}
+}
---
+++ Assets/Scripts/ClientScripts/Monster/Walker_C.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walker_C : ClientMonster {
+	public WalkerGraphicController gcWalker;
+
+	//private 
+
+	public override void OnRequested (){
+		base.OnRequested();
+
+		StartCoroutine(WakeUpRoutine());
+	}
+
+	private IEnumerator WakeUpRoutine(){
+		// 일어난 뒤 1sec > 애니메이션 재생 > 1sec > 무적판정 끝
+		// wakeup animation 재생 중 바로 죽을때 애니메이션이 씹히는 이슈 때문
+
+		yield return new WaitForSeconds(1);
+
+		gcWalker.WakeUp ();
+
+		yield return new WaitForSeconds (1);
+
+		hTrigger.gameObject.SetActive (true);
+	}
+
+	public override void OnDie (){
+		base.OnDie ();
+
+		gcWalker.Die();
+
+		ReturnObject(8);
+	}
+
+	protected override void MonsterFreeze(){
+		gcWalker.AnimationFreeze ();
+	}
+
+	protected override void MonsterFreezeEnd(){
+		gcWalker.AnimationResume ();
+	}
+}
