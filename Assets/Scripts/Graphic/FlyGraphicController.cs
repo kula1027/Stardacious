7a334329c28773cfe89a8125e84c6a2e638f02cd
wonjaechoi <wvvwvwwv@naver.com>
2017-02-19 19:26:37 +0900

    몬스터

---
+++ Assets/Scripts/Graphic/FlyGraphicController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyGraphicController : MonsterGraphicCtrl {
+
+	void Awake(){
+		animator = transform.FindChild ("Offset").FindChild ("Pivot").GetComponent<Animator> ();
+		unitParts = GetComponentsInChildren<SpriteRenderer>();
+	}
+
+	public override void Initialize (){
+		throw new System.NotImplementedException ();
+	}
+
+	public void AnimationFreeze(){
+		animator.enabled = false;
+	}
+
+	public void AnimationResume(){
+		animator.enabled = true;
+	}
+
+	public override void Jump (){
+
+	}
+
+	public override void Attack (){
+		
+	}
+
+	public override void Walk (){
+		
+	}
+
+	public override void Idle (){
+		
+	}
+
+	public override void Die (){
+
+	}
+
+	public void WakeUp(){
+
+	}
+}
