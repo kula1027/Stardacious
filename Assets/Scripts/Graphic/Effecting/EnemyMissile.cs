c935c7bf7d2ce6ab93c8e85efe594ade594fc3df
wonjaechoi <wvvwvwwv@naver.com>
2017-02-23 10:24:18 +0900

    고리와랜 재미없음

---
+++ Assets/Scripts/Graphic/Effecting/EnemyMissile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMissile : MonoBehaviour {
+	private SpriteRenderer spriteRenderer;
+
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		Init ();
+	}
+
+	void Init(){
+		StartCoroutine (Twinkle ());
+	}
+
+	IEnumerator Twinkle(){
+		float timer = 0f;
+		while (true) {
+			timer = 0;
+			while (true) {
+				yield return null;
+				timer += Time.deltaTime;
+				spriteRenderer.color = new Color (1 - timer * 2, 0, 0, 1);
+				if (timer > 0.5f) {
+					break;
+				}
+			}
+			timer = 0;
+			while (true) {
+				yield return null;
+				timer += Time.deltaTime;
+				spriteRenderer.color = new Color (timer * 2, 0, 0, 1);
+				if (timer > 0.5f) {
+					break;
+				}
+			}
+		}
+	}
+}
