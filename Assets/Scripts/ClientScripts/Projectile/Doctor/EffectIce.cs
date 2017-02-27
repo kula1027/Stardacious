685e73fb3dab4d6ba48a047e96f5ddf15fe91817
wonjaechoi <wvvwvwwv@naver.com>
2017-02-28 01:31:45 +0900

    마이너 버그

--- Assets/Scripts/ClientScripts/Projectile/Doctor/EffectIce.cs
+++ Assets/Scripts/ClientScripts/Projectile/Doctor/EffectIce.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class EffectIce : PoolingObject {
-
+	public AudioClip audioIce;
 	public override void OnRequested (){
+		MakeSound (audioIce);
 		ReturnObject(CharacterConst.Doctor.freezeTime);
 	}
 }
