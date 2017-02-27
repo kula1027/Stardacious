14dd20ca6df379a880c4ad56aa1d5094d588d3b4
wonjaechoi <wvvwvwwv@naver.com>
2017-02-27 23:51:58 +0900

    브금추가

--- Assets/Scripts/ClientScripts/Camera/CameraFollowTrigger.cs
+++ Assets/Scripts/ClientScripts/Camera/CameraFollowTrigger.cs
@@ -6,6 +6,21 @@ public class CameraFollowTrigger : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col){
 		CameraControl.instance.FollowMode();
 		inputBlock.SetActive (true);
+		StartCoroutine (BgmFadeOut ());
+	}
+	IEnumerator BgmFadeOut(){
+		float timer = 0;
+		AmbientSoundManager soundManager = AmbientSoundManager.instance;
+		while (true) {
+			timer += Time.deltaTime;
+			soundManager.bgmSource.volume = 1 - timer;
+			if (timer > 1) {
+				break;
+			}
+			yield return null;
+		}
+		soundManager.BgmPlay (null);
+		soundManager.bgmSource.volume = 1;
 	}
 
 	void OnTriggerExit2D(Collider2D col){
